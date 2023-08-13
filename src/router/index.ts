d1b30700b20356ea926ce79efc15291fea068bd0
Igor Libukin <ulterpwnz@list.ru>
2023-08-13 11:59:53 +0300

    add login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Register from '@/public/Register.vue';
+import Login from '@/public/Login.vue';
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  {path: '/register', component: Register},
+  {path: '/login', component: Login},
+
 ]
 
 const router = createRouter({
